1fd5d37480fefc05bb2567e33fb675a0c4ce0a3a
michael <scottrodriguez2k@gmail.com>
2024-08-30 11:35:55 -0600

    feat: connection nats

--- src/modules/document-generator/document-generator.module.ts
+++ src/modules/document-generator/document-generator.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DocumentGeneratorController } from './document-generator.controller';
+import { NatsModule } from '../transports/nats.module';
 
 @Module({
   controllers: [DocumentGeneratorController],
+  imports: [NatsModule],
 })
 export class DocumentGeneratorModule {}
