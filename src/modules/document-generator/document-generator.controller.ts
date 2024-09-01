515a0702aa5fea0f3efcbc550147db530bc62a5e
michael <scottrodriguez2k@gmail.com>
2024-09-01 17:19:12 -0600

    chore: probando mensajes entre microservicios

--- src/modules/document-generator/document-generator.controller.ts
+++ src/modules/document-generator/document-generator.controller.ts
@@ -1,16 +1,39 @@
 import { NATS_SERVICE } from '@app/config';
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, Logger } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
+const responseChannel = 'document:generated';
+interface DocumentGenerationResponse {
+  filename: string;
+  data: { name: string };
+  responseTo: string;
+}
 @Controller('document-generator')
 export class DocumentGeneratorController {
+  private readonly logger = new Logger(DocumentGeneratorController.name);
+
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Get()
-  generateDocuments() {
+  async generateDocuments() {
     try {
-      return this.client.send('generate-documents', {});
+      // Send the request to the NATS service
+      const response$ = await firstValueFrom(
+        this.client.send<DocumentGenerationResponse>('document:generate', {
+          fileName: 'test',
+          data: { name: 'John Doe' },
+          responseTo: responseChannel,
+        }),
+      );
+
+      this.logger.debug('Document generation request sent', {
+        response$,
+      });
+
+      return { message: 'Document generation request sent' };
     } catch (error) {
+      this.logger.error('Error generating document', error);
       throw new RpcException(error);
     }
   }
