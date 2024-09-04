4b9cfaabd828c17d957b8b1b7d815f235471ec74
xScottRodriguez <scottRodriguez2k@gmail.com>
2024-09-04 13:58:41 -0600

    chore: response ok

--- src/modules/document-generator/document-generator.controller.ts
+++ src/modules/document-generator/document-generator.controller.ts
@@ -13,7 +13,7 @@ import {
   ParseIntPipe,
   Query,
 } from '@nestjs/common';
-import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 import { QueryKeyDto } from './dto/query.dto';
 
@@ -30,61 +30,45 @@ export class DocumentGeneratorController {
 
   @Get()
   async generateDocuments() {
-    try {
-      // Send the request to the NATS service
-      const response$ = await firstValueFrom(
-        this.client.send<DocumentGenerationResponse>(GENERATE_PDF, {
-          from: '2024-01-01',
-          to: '2024-01-31',
-          documentType: '00',
-          format: 'PDF',
-        }),
-      );
-      this.logger.debug('Document generation request sent', {
-        response$,
-      });
+    // Send the request to the NATS service
+    const response$ = await firstValueFrom(
+      this.client.send<DocumentGenerationResponse>(GENERATE_PDF, {
+        from: '2024-01-01',
+        to: '2024-01-31',
+        documentType: '00',
+        format: 'PDF',
+      }),
+    );
 
-      return { message: 'Document generation request sent', response$ };
-    } catch (error) {
-      this.logger.error('Error generating document', error.mesage);
-      throw new RpcException(error);
-    }
+    return { message: 'Document generation request sent', data: response$ };
   }
   @Get('status/:jobId')
   async getDocumentGenerationStatus(
     @Param('jobId', ParseIntPipe) jobId: number,
   ) {
-    try {
-      const response = await firstValueFrom(
-        this.client.send(ZIP_STATUS, {
-          jobId,
-        }),
-      );
-      const data = await firstValueFrom(
-        this.client.send(ZIP_COMPLETED, response),
-      );
-      return data;
-    } catch (error) {
-      throw new RpcException(error);
-    }
+    this.logger.warn('Get document generation status request received', {
+      jobId,
+    });
+    const response = await firstValueFrom(
+      this.client.send(ZIP_STATUS, {
+        jobId,
+      }),
+    );
+    return response;
   }
   @Get('download-zip')
   async downloadZip(@Query() queryDto: QueryKeyDto) {
-    try {
-      const response = await firstValueFrom(
-        this.client.send(ZIP_COMPLETED, {
-          key: queryDto.fileName,
-        }),
-      );
-      this.logger.debug('Download zip request sent', {
-        response,
-      });
-      return {
-        downloadUrl: response,
-        expiresIn: '300S (5 minutes)',
-      };
-    } catch (error) {
-      throw new RpcException(error);
-    }
+    const response = await firstValueFrom(
+      this.client.send(ZIP_COMPLETED, {
+        key: queryDto.fileName,
+      }),
+    );
+    this.logger.debug('Download zip request sent', {
+      response,
+    });
+    return {
+      downloadUrl: response,
+      expiresIn: '300S (5 minutes)',
+    };
   }
 }
